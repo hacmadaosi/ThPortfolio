1bfe49c849c49c26284c09b5a3870d8e3b785215
hacmadaosi <lekhanhvinhnecon@gmail.com>
2025-09-20 13:19:37 +0700

    Thêm .gitignore và api môi trường

--- index.js
+++ index.js
@@ -12,6 +12,18 @@ app.get("/", (req, res) => {
   res.send("Hello world");
 });
 
+app.get("/firebase-api", (req, res) => {
+  res.json({
+    apiKey: process.env.VITE_FIREBASE_API_KEY,
+    authDomain: process.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.VITE_FIREBASE_APP_ID,
+    measurementId: process.env.VITE_FIREBASE_MEASUREMENT_ID,
+  });
+});
+
 app.get("/test", (req, res) => {
   res.json({ test: "test api" });
 });
