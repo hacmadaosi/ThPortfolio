ac8c4c8803066d62497b07f5361250cce4686234
hacmadaosi <lekhanhvinhnecon@gmail.com>
2025-09-20 13:38:45 +0700

    Test kết quả api lên giao diên

--- js/features.js
+++ js/features.js
@@ -1,19 +1,29 @@
-import { db } from "../../Server/firebase_config.js";
-import {
-  collection,
-  addDoc,
-} from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
+// import { db } from "../../Server/firebase_config.js";
+// import {
+//   collection,
+//   addDoc,
+// } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
 
-export async function addSampleData() {
+// export async function addSampleData() {
+//   try {
+//     const docRef = await addDoc(collection(db, "users"), {
+//       name: "Nguyễn Văn A",
+//       email: "vana@example.com",
+//       createdAt: new Date(),
+//     });
+//     console.log("Thêm thành công, ID: ", docRef.id);
+//   } catch (e) {
+//     console.error("Lỗi khi thêm dữ liệu: ", e);
+//   }
+// }
+// window.addSampleData = addSampleData;
+const localConfig = async () => {
   try {
-    const docRef = await addDoc(collection(db, "users"), {
-      name: "Nguyễn Văn A",
-      email: "vana@example.com",
-      createdAt: new Date(),
-    });
-    console.log("Thêm thành công, ID: ", docRef.id);
-  } catch (e) {
-    console.error("Lỗi khi thêm dữ liệu: ", e);
+    const res = await fetch("https://thportfolio.onrender.com/firebase-api");
+    const data = await res.json();
+    document.getElementById("config").innerText = JSON.stringify(data, null, 2);
+  } catch (ex) {
+    console.error("Lỗi gọi API:", ex);
   }
-}
-window.addSampleData = addSampleData;
+};
+localConfig();
