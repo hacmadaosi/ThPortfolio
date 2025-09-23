b3692cbf5ab4d9367d0da996e71a6a2b4417fc28
hacmadaosi <lekhanhvinhnecon@gmail.com>
2025-09-23 21:17:32 +0700

    Cập nhật lần 1

---
+++ front-end/js/app.js
@@ -0,0 +1,25 @@
+async function loadConfig() {
+  try {
+    const res = await fetch("https://thportfolio.onrender.com/firebase-api");
+    return await res.json();
+  } catch (err) {
+    console.error("Lỗi gọi API:", err);
+    return null;
+  }
+}
+
+const initFirebase = async () => {
+  const config = await loadConfig();
+
+  if (!config) {
+    alert("Không lấy được Firebase config");
+    return;
+  }
+  firebase.initializeApp(config);
+  const ui = new firebaseui.auth.AuthUI(firebase.auth());
+  ui.start("#firebaseui-auth-container", {
+    signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
+    signInSuccessUrl: "/",
+  });
+};
+document.addEventListener("DOMContentLoaded", initFirebase);
