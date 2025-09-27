cb498f21c0c83607b805a12cf62ca6abfd7dfdf7
hacmadaosi <lekhanhvinhnecon@gmail.com>
2025-09-28 01:08:19 +0700

    Sửa đường dẫn API

--- ui/api.endpoints.js
+++ ui/api.endpoints.js
@@ -5,6 +5,7 @@ export const API_END_POINTS = {
   },
   USER: {
     CREATE_USER: "aHR0cHM6Ly90aHBvcnRmb2xpby5vbnJlbmRlci5jb20vdXNlci9hZGQ=",
+    CHECK_LOGIN: "aHR0cHM6Ly90aHBvcnRmb2xpby5vbnJlbmRlci5jb20vbG9naW4=",
   },
 };
 export default API_END_POINTS;
