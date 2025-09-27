81bc2c3fee081598ca28a5c6c9eb544b9fb7b530
hacmadaosi <lekhanhvinhnecon@gmail.com>
2025-09-27 11:52:27 +0700

    Cấu trúc lại file UI_Controller, mã hóa chuỗi API

---
+++ ui/api.endpoints.js
@@ -0,0 +1,10 @@
+export const API_END_POINTS = {
+  PROJECT: {
+    GET_INFO_DEVELOPER:
+      "aHR0cHM6Ly90aHBvcnRmb2xpby5vbnJlbmRlci5jb20vZ2V0QWxsUHJvamVjdE1lbWJlcnM=",
+  },
+  USER: {
+    CREATE_USER: "aHR0cHM6Ly90aHBvcnRmb2xpby5vbnJlbmRlci5jb20vdXNlci9hZGQ=",
+  },
+};
+export default API_END_POINTS;
