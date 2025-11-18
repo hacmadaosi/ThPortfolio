9c5584a8bffa4799f69dd338c31523a5ed9b8822
hacmadaosi <lekhanhvinhnecon@gmail.com>
2025-11-18 19:15:17 +0700

    Sửa lại deploy

---
+++ frontend/api.endpoints.js
@@ -0,0 +1,11 @@
+export const API_END_POINTS = {
+  PROJECT: {
+    GET_INFO_DEVELOPER:
+      "aHR0cHM6Ly90aHBvcnRmb2xpby5vbnJlbmRlci5jb20vZ2V0QWxsUHJvamVjdE1lbWJlcnM=",
+  },
+  USER: {
+    CREATE_USER: "aHR0cHM6Ly90aHBvcnRmb2xpby5vbnJlbmRlci5jb20vdXNlci9hZGQ=",
+    CHECK_LOGIN: "aHR0cHM6Ly90aHBvcnRmb2xpby5vbnJlbmRlci5jb20vbG9naW4=",
+  },
+};
+export default API_END_POINTS;
